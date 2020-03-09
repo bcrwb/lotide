baf1cce5b4e9233f9ae12266fbf992df4eded068
Brent <brentcarey1@outlook.com>
2020-03-09 18:15:33 +0000

    finished middleTest.js

--- middle.js
+++ middle.js
@@ -1,23 +1,26 @@
-const eqArrays = function(array1,array2){
-    let track = null;
-  for(let i = 0; i <array1.length;i++){
-     for(let j = 0;j<array2.length;j++){
-       if(array1[i] === array2[i]){
-         track =  true
-       } else {
-          track = false
-       }
-     }
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+
+// const eqArrays = function(array1,array2){
+//     let track = null;
+//   for(let i = 0; i <array1.length;i++){
+//      for(let j = 0;j<array2.length;j++){
+//        if(array1[i] === array2[i]){
+//          track =  true
+//        } else {
+//           track = false
+//        }
+//      }
       
    
-    }
-    return track
-  }
+//     }
+//     return track
+//   }
 
-  const assertArraysEqual = function(arrayOne,arrayTwo){
-    eqArrays(arrayOne,arrayTwo) ? console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`) : console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+  // const assertArraysEqual = function(arrayOne,arrayTwo){
+  //   eqArrays(arrayOne,arrayTwo) ? console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`) : console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
   
-  };
+  // };
 
   const middle = function(array){
       if(array.length < 3){
@@ -30,11 +33,13 @@ const eqArrays = function(array1,array2){
 
   }
 
-console.log(middle([1])) 
-console.log(middle([1, 2]))
+// console.log(middle([1])) 
+// console.log(middle([1, 2]))
 
-console.log(middle([1, 2, 3])) 
-console.log(middle([1, 2, 3, 4, 5])) 
+// console.log(middle([1, 2, 3])) 
+// console.log(middle([1, 2, 3, 4, 5])) 
 
-assertArraysEqual(middle([1, 2, 3, 4]),[2,3] );
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]);
+// assertArraysEqual(middle([1, 2, 3, 4]),[2,3] );
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]);
+
+module.exports = middle;
